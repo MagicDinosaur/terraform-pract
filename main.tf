8328a09c08a32afe2671cc3d3d13bee6eb55538a
MagicDinosaur <phamvduc2112@gmail.com>
2023-07-07 03:21:19 -0400

    last commit

--- main.tf
+++ main.tf
@@ -8,3 +8,23 @@ provider "google" {
 # resource "google_compute_instance" "vm_instance" {
   
 # }
+
+
+#setting backend for terraform state
+resource "google_storage_bucket" "terraform_state_bucket" {
+  name          = "ducpham-gcp-practice-terraform-state"
+  force_destroy = true
+  location      = "US"
+  storage_class = "STANDARD"
+  versioning {
+    enabled = true
+  }
+}
+
+#### ASK SOMEONE ABOUT THIS!!! ANY ALTERNATIVE WAY INSTEAD OF RUNING APPLY 2 TIMES?
+# terraform {
+#   backend "gcs" {
+#     bucket = "ducpham-gcp-practice-terraform-state"
+#     prefix = "terraform/state"
+#   }
+# }
